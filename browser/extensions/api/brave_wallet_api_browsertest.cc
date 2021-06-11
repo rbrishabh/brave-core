4559794bffea411320d2531e54cafb049114ca46
Mario Sanchez Prada <mario@igalia.com>
2021-06-11 16:09:27 +0200

    Adat to deprecation of base::ScopedObserver
    base::ScopedObserver is being deprecated on Chromium 93, so we need
    to migrate to either ScopedObservation or ScopedMultiSourceObservation
    depending on whether one or multiple sources are being observed.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/3a35c513e236f28ba817b6667c9c973163241ebd
    
    commit 3a35c513e236f28ba817b6667c9c973163241ebd
    Author: Sigurdur Asgeirsson <siggi@chromium.org>
    Date:   Thu Jun 3 16:34:54 2021 +0000
    
        Remove redundant base/scoped_observer.h includes.
    
        ScopedObserver is being deprecated in favor of two new classes:
        - base::ScopedObservation for observers that only ever observe
          a single source.
        - base::ScopedMultiSourceObservation for observers that do or may
          observe more than a single source.
    
        Bug: 1145565
    
    Resolves https://github.com/brave/brave-browser/issues/16371

--- browser/extensions/api/brave_wallet_api_browsertest.cc
+++ browser/extensions/api/brave_wallet_api_browsertest.cc
@@ -4,7 +4,7 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "base/path_service.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_multi_source_observation.h"
 #include "brave/browser/ethereum_remote_client/ethereum_remote_client_constants.h"
 #include "brave/browser/infobars/crypto_wallets_infobar_delegate.h"
 #include "brave/common/brave_paths.h"
@@ -35,9 +35,7 @@ namespace extensions {
 class BraveWalletAPIBrowserTest : public InProcessBrowserTest,
     public InfoBarManager::Observer {
  public:
-  BraveWalletAPIBrowserTest() : infobar_observer_(this),
-      infobar_added_(false) {
-  }
+  BraveWalletAPIBrowserTest() : infobar_added_(false) {}
 
   void WaitForBraveExtensionAdded() {
     // Brave extension must be loaded, otherwise dapp detection events
@@ -105,11 +103,11 @@ class BraveWalletAPIBrowserTest : public InProcessBrowserTest,
   }
 
   void AddInfoBarObserver(InfoBarService* infobar_service) {
-    infobar_observer_.Add(infobar_service);
+    infobar_observer_.AddObservation(infobar_service);
   }
 
   void RemoveInfoBarObserver(InfoBarService* infobar_service) {
-    infobar_observer_.Remove(infobar_service);
+    infobar_observer_.AddObservation(infobar_service);
   }
 
   content::WebContents* active_contents() {
@@ -160,8 +158,8 @@ class BraveWalletAPIBrowserTest : public InProcessBrowserTest,
 
  private:
   scoped_refptr<const extensions::Extension> extension_;
-  ScopedObserver<InfoBarManager, InfoBarManager::Observer>
-      infobar_observer_;
+  base::ScopedMultiSourceObservation<InfoBarManager, InfoBarManager::Observer>
+      infobar_observer_{this};
   bool infobar_added_;
   std::unique_ptr<base::RunLoop> infobar_added_run_loop_;
 
